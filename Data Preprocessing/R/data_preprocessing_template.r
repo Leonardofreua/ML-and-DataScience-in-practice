158c31a19a70fba68d9e5e0d53c28aec8c24c026
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-03 16:25:30 -0300

    Taking care of missing data in R

--- Data Preprocessing/R/data_preprocessing_template.r
+++ Data Preprocessing/R/data_preprocessing_template.r
@@ -1,6 +1,16 @@
 # Data Preprocessing
 
-#Importing the dataset
+# Setting Work Directory
 dir <- paste0(getwd(), '/Data Preprocessing/R')
 setwd(dir)
+
+# Importing the dataset
 dataset = read.csv("Data.csv")
+
+# Taking care of missing data
+dataset$Age =  ifelse(is.na(dataset$Age),
+                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Age)
+dataset$Salary =  ifelse(is.na(dataset$Salary),
+                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                         dataset$Salary)
