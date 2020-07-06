d5b75be5058b50e5b026b350d663d0608cb7d9fb
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-06 19:27:44 -0300

    Splitting the dataset into the Training set and Test set in R

--- Data Preprocessing/R/data_preprocessing_template.r
+++ Data Preprocessing/R/data_preprocessing_template.r
@@ -20,5 +20,14 @@ dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1, 2, 3))
 dataset$Purchased = factor(dataset$Purchased,
-                         levels = c('No', 'Yes'),
-                         labels = c(0, 1))
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
