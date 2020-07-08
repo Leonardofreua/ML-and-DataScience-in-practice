0a850eb914fde60449973f993604e17834a7d754
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-08 20:04:21 -0300

    Starting Simple Linear Regression in R

---
+++ Regression/Simple Linear Regression/R/simple_linear_regression.R
@@ -0,0 +1,12 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
