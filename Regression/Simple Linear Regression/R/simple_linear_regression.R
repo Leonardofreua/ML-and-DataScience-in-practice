5566c8b199df881fd181787b001ea3ea041228eb
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-08 20:25:41 -0300

    Fitting Simple Linear Regression to the Training set in R

--- Regression/Simple Linear Regression/R/simple_linear_regression.R
+++ Regression/Simple Linear Regression/R/simple_linear_regression.R
@@ -10,3 +10,7 @@ set.seed(123)
 split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+# Fitting Simple Linear Regression to the Training set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
