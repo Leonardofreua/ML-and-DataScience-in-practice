3275df01ebf4a08a0a1de1432e6bb95413dc6cbc
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-09 12:44:02 -0300

    Visualising the Training and Test set results in R

--- Regression/Simple Linear Regression/R/simple_linear_regression.R
+++ Regression/Simple Linear Regression/R/simple_linear_regression.R
@@ -17,3 +17,26 @@ regressor = lm(formula = Salary ~ YearsExperience,
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# Visualising the Training set results
+# install.packages('ggplot2')
+library(ggplot2)
+
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+               colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+              colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
+
+# Visualising the Test set results
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of experience') +
+  ylab('Salary')
