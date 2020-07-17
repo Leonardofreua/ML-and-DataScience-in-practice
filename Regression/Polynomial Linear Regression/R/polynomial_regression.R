af1c3b960b7757809b9de6e372391179f1959897
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-17 12:24:34 -0300

    Visualising the Polynomial Regression results in R

--- Regression/Polynomial Linear Regression/R/polynomial_regression.R
+++ Regression/Polynomial Linear Regression/R/polynomial_regression.R
@@ -16,5 +16,28 @@ lin_reg = lm(formula = Salary ~ .,
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
+
+# Visualising the Linear Regression results
+# installed.packages('ggplot2')
+  library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+           colour = 'blue') +
+  ggtitle('Truth or Bluff (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualising the Polynomial Regression results
+  ggplot() +
+    geom_point(aes(x = dataset$Level, y = dataset$Salary),
+               colour = 'red') +
+    geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+              colour = 'blue') +
+    ggtitle('Truth or Bluff (Polynomial Regression)') +
+    xlab('Level') +
+    ylab('Salary')
