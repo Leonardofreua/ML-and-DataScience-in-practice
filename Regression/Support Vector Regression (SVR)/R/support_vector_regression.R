85bc5b2ef9df07e5722610a442772d4250225e7e
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-07-20 15:49:15 -0300

    Implemented the SVR in R

---
+++ Regression/Support Vector Regression (SVR)/R/support_vector_regression.R
@@ -0,0 +1,27 @@
+# Support Vector Regression
+
+# Importing the dataset
+dataset = read.csv('../../../Datasets/Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Fitting the SVR to the dataset
+# install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
+
+# Predicting a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualising the SVR results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (SVR)') +
+  xlab('Level') +
+  ylab('Salary')
